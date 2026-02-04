33a91e19c235a4fd3191944bd9158e1bbd46da82
Zed Wong <kingflyme1@gmail.com>
2026-02-04 22:31:18 +0800

    update css

--- interface/src/lib/components/wallet/balance/title.svelte
+++ interface/src/lib/components/wallet/balance/title.svelte
@@ -1,42 +1,36 @@
 <script lang="ts">
-  import { formatDecimals } from "$lib/helpers/utils";
-  import { currencySymbol, userAssets } from "$lib/stores/wallet";
+    import { formatDecimals } from "$lib/helpers/utils";
+    import { currencySymbol, userAssets } from "$lib/stores/wallet";
 </script>
 
 <div class="flex flex-col">
-  <!-- Dollar value title -->
-  <div class="relative flex justify-center my-8 mb-3 space-x-2 mx-auto">
-    <div class="absolute left-[-6px]">
-      <span class="text-base balance-font opacity-30">
-        {$currencySymbol}
-      </span>
+    <!-- Dollar value title -->
+    <div class="relative flex justify-center my-8 mb-3 space-x-2 mx-auto">
+        <div class="absolute -left-1.5">
+            <span class="text-base balance-font opacity-30">
+                {$currencySymbol}
+            </span>
+        </div>
+        {#if $userAssets.totalUSDBalance}
+            <span class="text-4xl font-medium balance-font">
+                {$userAssets.totalUSDBalance <= 1
+                    ? formatDecimals($userAssets.totalUSDBalance, 4)
+                    : formatDecimals($userAssets.totalUSDBalance, 2)}
+            </span>
+        {:else}
+            <span class="text-4xl font-medium balance-font"> 0 </span>
+        {/if}
     </div>
-    {#if $userAssets.totalUSDBalance}
-      <span class="text-4xl font-medium balance-font">
-        {
-          $userAssets.totalUSDBalance <= 1 ? formatDecimals($userAssets.totalUSDBalance, 4) : formatDecimals($userAssets.totalUSDBalance, 2)
-        }
-      </span>
-    {:else}
-      <span class="text-4xl font-medium balance-font">
-        0
-      </span>
-    {/if}
-  </div>
 
-  <!-- BTC Value -->
-  <div class="flex justify-center items-center space-x-2">
-    {#if $userAssets.totalBTCBalance}
-      <span class="balance-font">
-        {formatDecimals($userAssets.totalBTCBalance, 8)}
-      </span>
-    {:else}
-      <span class="balance-font">
-        0
-      </span>
-    {/if}
-    <span class="text-xs opacity-50">
-      BTC 
-    </span>
-  </div>
+    <!-- BTC Value -->
+    <div class="flex justify-center items-center space-x-2">
+        {#if $userAssets.totalBTCBalance}
+            <span class="balance-font">
+                {formatDecimals($userAssets.totalBTCBalance, 8)}
+            </span>
+        {:else}
+            <span class="balance-font"> 0 </span>
+        {/if}
+        <span class="text-xs opacity-50"> BTC </span>
+    </div>
 </div>
