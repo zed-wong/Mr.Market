48ae2ad8004f27c5f83b4bfa900798c36603f039
Zed Wong <kingflyme1@gmail.com>
2026-02-05 16:06:09 +0800

    Update interface e2e tests

--- interface/tests/home/home.spec.ts
+++ interface/tests/home/home.spec.ts
@@ -13,10 +13,8 @@ test('bottom navigation', async ({ page }) => {
   await page.waitForURL('**/home');
   await page.getByTestId('bottom-nav-market').click();
   await page.waitForURL('**/market/**');
-  await page.getByTestId('bottom-nav-trade').click();
-  await page.waitForURL('**/spot/**');
-  await page.getByTestId('bottom-nav-grow').click();
-  await page.waitForURL('**/grow');
+  await page.getByTestId('bottom-nav-market-making').click();
+  await page.waitForURL('**/market-making');
   await page.getByTestId('bottom-nav-wallet').click();
   await page.waitForURL('**/wallet');
 })
