0202e5b138de9947fc4d3e8cada9e3fbf0bceb7e
Zed Wong <36271125+zed-wong@users.noreply.github.com>
2026-02-20 19:10:50 +0800

    fix add pair field overlap on smaller screens

--- interface/src/lib/components/admin/settings/spotTrading/AddTradingPair.svelte
+++ interface/src/lib/components/admin/settings/spotTrading/AddTradingPair.svelte
@@ -362,7 +362,7 @@ import { toast } from "svelte-sonner";
             </div>
           </div>
         {:else}
-          <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div class="grid grid-cols-1 xl:grid-cols-2 gap-4">
             <div class="form-control w-full col-span-2">
           <label class="label" for="exchange-id-input">
             <span class="label-text font-medium">{$_("exchange_id")}</span>
