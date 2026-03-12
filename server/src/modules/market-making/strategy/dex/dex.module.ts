473be301be4febdbc035800ac47a72500b820316
Zed Wong <kingflyme1@gmail.com>
2026-03-12 09:59:48 +0800

    refactor(server): dedupe defi providers in dex module

--- server/src/modules/market-making/strategy/dex/dex.module.ts
+++ server/src/modules/market-making/strategy/dex/dex.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { DexAdapterRegistry } from 'src/modules/defi/adapter-registry';
-import { PancakeV3Adapter } from 'src/modules/defi/adapters/pancakeV3.adapter';
-import { UniswapV3Adapter } from 'src/modules/defi/adapters/uniswapV3.adapter';
 import { DefiModule } from 'src/modules/defi/defi.module';
 
 import { Web3Module } from '../../../web3/web3.module';
@@ -9,17 +6,7 @@ import { DexVolumeStrategyService } from './dex-volume.strategy.service';
 
 @Module({
   imports: [Web3Module, DefiModule],
-  providers: [
-    UniswapV3Adapter,
-    PancakeV3Adapter,
-    DexAdapterRegistry,
-    DexVolumeStrategyService,
-  ],
-  exports: [
-    UniswapV3Adapter,
-    PancakeV3Adapter,
-    DexAdapterRegistry,
-    DexVolumeStrategyService,
-  ],
+  providers: [DexVolumeStrategyService],
+  exports: [DefiModule, DexVolumeStrategyService],
 })
 export class DexModule {}
